7d88160079072573ce92b23340644b1c3533353a
alpsarikisla <50963484+alpsarikisla@users.noreply.github.com>
2025-04-06 16:14:09 +0300

    Proje Tamamlandi

---
+++ MusiCodeWebApp/Models/ViewModels/PaymentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusiCodeWebApp.Models.ViewModels
+{
+    public class PaymentViewModel
+    {
+        [Required(ErrorMessage = "Bu alan zorunlu")]
+        public string NameSurname { get; set; }
+
+        [Required(ErrorMessage ="Bu alan zorunlu")]
+        public string CardNumber { get; set; }
+
+        [Required(ErrorMessage = "Bu alan zorunlu")]
+        public string ReqMonth { get; set; }
+
+        [Required(ErrorMessage = "Bu alan zorunlu")]
+        public string ReqYear { get; set; }
+
+        [Required(ErrorMessage = "Bu alan zorunlu")]
+        public string Cvv { get; set; }
+    }
+}
